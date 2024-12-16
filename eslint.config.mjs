527eb912ef8d31ebafb58e6579b2211bb57c0713
Manish Beesetti <pavan.manish9@gmail.com>
2024-12-16 14:05:07 -0500

    fix docker file pnpm

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,54 +1,41 @@
-import antfu from "@antfu/eslint-config";
-
-export default antfu(
-  {
-    type: "app",
-    typescript: true,
-    formatters: true,
-    react: true,
-    jsonc: true,
-    stylistic: {
-      indent: "tab", // Tab indentation
-      semi: true, // Enforce semicolons
-      quotes: "double", // Use double quotes
-    },
-    ignores: ["tailwind.config.ts"], // Ignore specific file
-  },
-  {
-    rules: {
-      // Disable all ESLint rules
-      "no-console": "off",
-      "unused-imports/no-unused-imports": "off",
-      "unused-imports/no-unused-vars": "off",
-      "node/prefer-global/process": "off",
-      "ts/no-use-before-define": "off",
-      "react/prefer-destructuring-assignment": "off",
-      "react-dom/no-missing-button-type": "off",
-      "react-hooks/exhaustive-deps": "off",
-      "react-refresh/only-export-components": "off",
-      "react/no-unstable-context-value": "off",
-      "style/indent": "off",
-      "style/multiline-ternary": "off",
-      "react-hooks/rules-of-hooks": "off",
-      "no-unused-vars": "off",
-      "no-undef": "off",
-      "no-redeclare": "off",
-      "no-magic-numbers": "off",
-      "no-alert": "off",
-      "no-debugger": "off",
-      "react/jsx-key": "off",
-      "react/jsx-no-undef": "off",
-      "react/prop-types": "off",
-      "react/react-in-jsx-scope": "off",
-      "import/no-unresolved": "off",
-      "import/named": "off",
-      "import/default": "off",
-      "import/no-absolute-path": "off",
-      "import/no-dynamic-require": "off",
-      "import/no-named-as-default": "off",
-      "import/no-named-as-default-member": "off",
-      "import/first": "off",
-      "import/no-extraneous-dependencies": "off",
-    },
-  },
-);
+module.exports = {
+	root: true,
+	env: {
+		browser: true,
+		es2021: true,
+		node: true,
+	},
+	extends: [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended",
+		"plugin:react/recommended",
+		"plugin:react-hooks/recommended",
+		"plugin:jsx-a11y/recommended",
+		"next/core-web-vitals",
+	],
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 12,
+		sourceType: "module",
+	},
+	plugins: ["@typescript-eslint", "react", "react-hooks", "jsx-a11y"],
+	rules: {
+		indent: ["error", "tab"],
+		semi: ["error", "always"],
+		quotes: ["error", "double"],
+		"no-console": "off",
+		"@typescript-eslint/no-unused-vars": "off",
+		"react/prop-types": "off",
+		"react/react-in-jsx-scope": "off",
+		"react-hooks/exhaustive-deps": "off",
+	},
+	ignorePatterns: ["tailwind.config.ts"],
+	settings: {
+		react: {
+			version: "detect",
+		},
+	},
+};
